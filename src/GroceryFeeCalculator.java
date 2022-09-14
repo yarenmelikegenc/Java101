515b4a8b5c4eb4bbb7e7ac57f47f2580e2ccc4db
Yaren Melike Genc <gencyarenmelike@gmail.com>
2022-09-14 14:22:45 +0300

    Kaydedildi

---
+++ src/GroceryFeeCalculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class GroceryFeeCalculator {
+    public static void main(String[] args) {
+        double armutFiyat, armutKg, elmaFiyat, elmaKg, domatesFiyat, domatesKg, muzFiyat, muzKg, patlicanFiyat, patlicanKg,toplamTutar;
+
+        armutFiyat = 2.14;
+        elmaFiyat = 3.67;
+        domatesFiyat = 1.11;
+        muzFiyat = 0.95;
+        patlicanFiyat = 5.00;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kaç kilo Armut : ");
+        armutKg = input.nextDouble();
+        armutKg = armutKg * armutFiyat;
+
+        System.out.print("Kaç kilo Elma : ");
+        elmaKg = input.nextDouble();
+        elmaKg = elmaKg * elmaFiyat;
+
+        System.out.print("Kaç kilo Domates : ");
+        domatesKg = input.nextDouble();
+        domatesKg = domatesKg * domatesFiyat;
+
+        System.out.print("Kaç kilo Muz : ");
+        muzKg = input.nextDouble();
+        muzKg = muzKg * muzFiyat;
+
+        System.out.print("Kaç kilo Patlıcan : ");
+        patlicanKg = input.nextDouble();
+        patlicanKg = patlicanKg * patlicanFiyat;
+
+        toplamTutar = (armutKg + elmaKg + domatesKg + muzKg + patlicanKg);
+        System.out.print("Toplam Tutar : " + toplamTutar + " TL");
+
+    }
+}
