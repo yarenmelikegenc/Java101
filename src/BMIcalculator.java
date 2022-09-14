50a94d9163b6338740f06031ba2d2596222a1275
Yaren Melike Genc <gencyarenmelike@gmail.com>
2022-09-14 14:12:11 +0300

    Kaydedildi

---
+++ src/BMIcalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BMIcalculator {
+    public static void main(String[] args) {
+        double kg,metre,vki;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Boyunuzı (metre cinsinden) giriniz: ");
+        metre = input.nextDouble();
+        System.out.println("Kilonuzu (kg cinsinden) giriniz: ");
+        kg = input.nextDouble();
+        vki = kg / (metre * metre);
+        System.out.println("Vücut kitle endeksiniz: " + vki);
+    }
+}
