0f081b9523b259567d29ede74101dbe052002c8d
Yaren Melike Genc <gencyarenmelike@gmail.com>
2022-09-19 14:48:17 +0300

    Kaydedildi

---
+++ src/PalindromNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromNumber {
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, lastNumber;
+
+        while (temp != 0) {
+            lastNumber = temp % 10;
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            temp /= 10;
+        }
+        if (number == reverseNumber) {
+            System.out.println("Palindrom sayıdır.");
+            return true;
+        } else {
+            System.out.println("Palindrom sayı değildir.");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir sayı girin:");
+        int n = inp.nextInt();
+        System.out.println(isPalindrom(n));
+
+
+    }
+}
