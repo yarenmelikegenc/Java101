99e08b2ef0f0658d7fbcc8903a7b69b746f75ecf
Yaren Melike Genc <gencyarenmelike@gmail.com>
2022-09-19 15:07:17 +0300

    Kaydedildi

---
+++ src/Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    static int fib(int n){
+        if(n == 1 || n ==2){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int n = input.nextInt();
+        fib(n);
+        System.out.println(fib(n));
+    }
+}
