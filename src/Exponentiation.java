b61f89a901962f543951dc9e3c8f327c3e00503a
Yaren Melike Genc <gencyarenmelike@gmail.com>
2022-09-19 16:07:42 +0300

    Kaydedildi

--- src/Exponentiation.java
+++ src/Exponentiation.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 
 public class Exponentiation {
 
-    public static void main(String[] args){
+    static int exp(int base, int pow) {
+        if (pow == 0)
+            return 1;
+        return base * exp(base, pow - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Taban sayısını giriniz: ");
+        int base = input.nextInt();
+
+        System.out.print("Üssü giriniz: ");
+        int pow = input.nextInt();
+
+        System.out.println("Result : " + exp(base, pow));
 
     }
 }
