7e5c44d1039b780dfefde26818847b4713d06e7a
Yaren Melike Genc <gencyarenmelike@gmail.com>
2022-09-30 17:24:35 +0300

    Kaydedildi

--- src/StarTriangle.java
+++ src/StarTriangle.java
@@ -2,12 +2,33 @@ import java.util.Scanner;
 
 public class StarTriangle {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        for(int row=1; row <= 6; row++){
-            for (int star = 1; star <= 6; star++) {
+        int n,a,b,c;
+        Scanner inp = new Scanner(System.in);
+        System.out.print(" Bir Sayı gırınız : ");
+        n = inp.nextInt();
+        for ( a = 1; a <= n; a++) {
+            for ( b = 1; b <= (n - a); b++) {
+                System.out.print(" ");
 
             }
+            for ( c = 1; c <= ( 2* a - 1 ); c++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+        for ( a = 1; a < n; a++) {
+            for ( b = 1; b <= a; b++) {
+                System.out.print(" ");
+
+            }
+            for ( c = 1; c <= ( 2 * n - (2 * a + 1) ); c++) {
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
         }
     }
 }
